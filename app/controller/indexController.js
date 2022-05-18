f023a5f7bc9f26b29e793d02912f102680d6dbe5
fabrizio sciascia <sciax@virgilio.it>
2022-05-18 11:47:00 +0200

    wqefqdf

--- app/controller/indexController.js
+++ app/controller/indexController.js
@@ -8,6 +8,133 @@
         console.log("Initialized");    }
 
     vm.HomePage = [
+
+        {
+            'ahref': '../Categorie/magliette-personalizzate.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/maglietta cotone bianca.jpg',
+            'imgAlt': 'maglietta cotone bianca personalizzata a 1 colore',
+            'prezzoVetrina': 'da € 2,30',
+            'titolo': 'Maglietta bianca personalizzata',
+            'descrizione': 'Maglietta bianca adulto / bambino 135 gr. in cotone 100%, girocollo a costina rinforzata, stampa a 1 colore da un lato'
+        },
+
+        {
+            'ahref': '../Categorie/magliette-personalizzate.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/maglietta cotone colorate.jpg',
+            'imgAlt': 'maglietta cotone colorata personalizzata a 1 colore',
+            'prezzoVetrina': 'da € 2,70',
+            'titolo': 'Maglietta colorata personalizzata',
+            'descrizione': 'Adulto / bambino 140 gr. in cotone 100%, 10 colori a scelta, girocollo a costina rinforzata, stampa a 1 colore da un lato'
+        },
+
+        {
+            'ahref': '../Categorie/Abbigliamento-da-lavoro.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/gilet-gialli.jpg',
+            'imgAlt': 'gilet classe 2',
+            'prezzoVetrina': 'da € 4,50',
+            'titolo': 'Gilet alta visibilità classe 2',
+            'descrizione': 'Tessuto poliestere colore arancio e giallo, taglie uniche, bande riflettenti 3M Scotchlite.Personalizzabili davanti e dietro'
+        },
+        {
+            'ahref': '../Categorie/Abbigliamento-da-lavoro.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/felpa-full-zip.jpg',
+            'imgAlt': 'felpa con cappuccio full zip',
+            'prezzoVetrina': 'da € 18',
+            'titolo': 'Felpa cappuccio personalizzata',
+            'descrizione': 'Felpa con cappuccio -  zip lunga - polsini e girovita elasticizzati - 2 tasche a marsupio - Ricamo del nome omaggio'
+        },
+
+        {
+            'ahref': '../Categorie/Borse-Zaini-personalizzati.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/sacca-a-felpa.gif',
+            'imgAlt': 'zainetti a forma di felpa',
+            'prezzoVetrina': 'da € 12,00',
+            'titolo': 'Sacca a felpa',
+            'descrizione': 'Sacca a forma di felpa grigia, con cerniera e laccetti nei colori blu, rosso , verde, nera. Personalizzata con ricamo'
+        },
+
+        {
+            'ahref': '../Categorie/Penne-personalizzate.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/Penne-personalizzate-a-1-colore.jpg',
+            'imgAlt': 'penna a biro personalizzata a 1 colore',
+            'prezzoVetrina': 'da € 0,30',
+            'titolo': 'Penna a sfera personalizzata',
+            'descrizione': 'Descrizione: Penna biro con stampa a 1 colore - Refil nero'
+        },
+
+        {
+            'ahref': '../Categorie/Borse-Zaini-personalizzati.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/zainetti.jpg',
+            'imgAlt': 'zainetti colorati personalizzati',
+            'prezzoVetrina': 'da € 5,90',
+            'titolo': 'Zainetti personalizzati',
+            'descrizione': 'Colori blu, rosso, viola, nero, azzurro, royal, grigio, f.to 26,5 x h 38 x 12 cm.. Personalizzabili davanti a 1 colore'
+        },
+
+
+        {
+            'ahref': '../Categorie/Braccialetti-personalizzati.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/bracciali-in-silicone.jpg',
+            'imgAlt': 'braccialetti in silicone personalizzati',
+            'prezzoVetrina': 'da € 0,66',
+            'titolo': 'Braccialetti in silicone',
+            'descrizione': 'Braccialetti in silicone a più colori, stampa a 1 colore inclusa'
+        },
+
+        {
+            'ahref': '../Categorie/Lanyard-personalizzati.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/lanyards-personalizzati.jpg',
+            'imgAlt': 'Lanyards personalizzati con logo fronte e retro',
+            'prezzoVetrina': 'da € 0,60',
+            'titolo': 'Lanyards personalizzati',
+            'descrizione': 'Lanyards personalizzato con il tuo logo aziendale stampato interno / esterno - Misura: 2 x 45-50 cm.'
+        },
+
+        {
+            'ahref': '../Categorie/Borse-Zaini-personalizzati.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/sacche economiche.jpg',
+            'imgAlt': 'sacche personalizzate',
+            'prezzoVetrina': 'da € 0,85',
+            'titolo': 'Sacca in nylon',
+            'descrizione': 'Dimensione cm 33x45 ca, doppia corda di chiusura, stampa a 1 colore su un lato'
+        },
+
+        {
+            'ahref': '../Categorie/Shopper-personalizzati.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/shopper cotone.jpg',
+            'imgAlt': 'shopper in cotone personalizzati',
+            'prezzoVetrina': 'da € 0,90',
+            'titolo': 'Shoppers in cotone',
+            'descrizione': 'Shopper in cotone naturale con manici colorati.F.to 40 x 30 cm. Personalizzati a 1 colore'
+        },
+
+        {
+            'ahref': '../Categorie/Borse-Zaini-personalizzati.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/cartella-porta-documenti.jpg',
+            'imgAlt': 'cartella porta documenti personalizzata a 1 colore',
+            'prezzoVetrina': 'da € 1,60',
+            'titolo': 'Cartella porta documenti',
+            'descrizione': 'Pratica cartella porta documenti, robusta in tessuto pesante personalizzata a 1 colore'
+        },
+
+        {
+            'ahref': '../Categorie/Braccialetti-personalizzati.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/bracciali-in-silicone-in-rilievo.jpg',
+            'imgAlt': 'braccialetti in silicone personalizzati in rilievo con lo stesso colore',
+            'prezzoVetrina': 'da € 0,78',
+            'titolo': 'Braccialetti in silicone',
+            'descrizione': 'Braccialetti in silicone a più colori, personalizzati in rilevo con lo stesso colore del braccialetto'
+        },
+
+        {
+            'ahref': '../Categorie/Braccialetti-personalizzati.aspx',
+            'imgSrc': '../../jmg/immaginiVetrina/bracciali-in-silicone-incisi.jpg',
+            'imgAlt': 'braccialetti in silicone personalizzati incisi con lo stesso colore',
+            'prezzoVetrina': 'da € 0,75',
+            'titolo': 'Braccialetti in silicone',
+            'descrizione': 'Braccialetti in silicone a più colori, personalizzati con incisione con lo stesso colore del braccialetto'
+        },
+
         {
             'ahref': '../Categorie/magliette-personalizzate.aspx',
             'imgSrc': '../../jmg/immaginiVetrina/mascherine-personalizzate.jpg',
@@ -81,15 +208,6 @@
         },
 
         {
-            'ahref': '../Categorie/Abbigliamento-da-lavoro.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/gilet-gialli.jpg',
-            'imgAlt': 'gilet classe 2',
-            'prezzoVetrina': 'da € 4,50',
-            'titolo': 'Gilet alta visibilità classe 2',
-            'descrizione': 'Tessuto poliestere colore arancio e giallo, taglie uniche, bande riflettenti 3M Scotchlite.Personalizzabili davanti e dietro'
-        },
-
-        {
             'ahref': '../Categorie/Agende-2021-personalizzate.aspx',
             'imgSrc': '../../jmg/immaginiVetrina/AGENDA-2020-giornaliera.jpg',
             'imgAlt': 'agenda giornaliera f.to 15 x 21',
@@ -143,42 +261,7 @@
             'descrizione': 'Gilet colore blu, taglie S-M-L-XL-XXL-3XL-4XL, bande riflettenti. Multi tasche. Ricamo del nome omaggio'
         },
 
-        {
-            'ahref': '../Categorie/Abbigliamento-da-lavoro.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/felpa-full-zip.jpg',
-            'imgAlt': 'felpa con cappuccio full zip',
-            'prezzoVetrina': 'da € 18',
-            'titolo': 'Felpa cappuccio personalizzata',
-            'descrizione': 'Felpa con cappuccio -  zip lunga - polsini e girovita elasticizzati - 2 tasche a marsupio - Ricamo del nome omaggio'
-        },
-
-        {
-            'ahref': '../Categorie/Borse-Zaini-personalizzati.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/sacca-a-felpa.gif',
-            'imgAlt': 'zainetti a forma di felpa',
-            'prezzoVetrina': 'da € 12,00',
-            'titolo': 'Sacca a felpa',
-            'descrizione': 'Sacca a forma di felpa grigia, con cerniera e laccetti nei colori blu, rosso , verde, nera. Personalizzata con ricamo'
-        },          
-        
-        {
-            'ahref': '../Categorie/Penne-personalizzate.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/Penne-personalizzate-a-1-colore.jpg',
-            'imgAlt': 'penna a biro personalizzata a 1 colore',
-            'prezzoVetrina': 'da € 0,30',
-            'titolo': 'Penna a sfera personalizzata',
-            'descrizione': 'Descrizione: Penna biro con stampa a 1 colore - Refil nero'
-        },
-
-        {
-            'ahref': '../Categorie/Borse-Zaini-personalizzati.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/zainetti.jpg',
-            'imgAlt': 'zainetti colorati personalizzati',
-            'prezzoVetrina': 'da € 5,90',
-            'titolo': 'Zainetti personalizzati',
-            'descrizione': 'Colori blu, rosso, viola, nero, azzurro, royal, grigio, f.to 26,5 x h 38 x 12 cm.. Personalizzabili davanti a 1 colore'
-        },
-
+     
         {
             'ahref': '../Categorie/Portachiavi-personalizzati.aspx',
             'imgSrc': '../../jmg/immaginiVetrina/portachiavi.jpg',
@@ -186,87 +269,6 @@
             'prezzoVetrina': 'da € 1,90',
             'titolo': 'Portachiavi in alluminio',
             'descrizione': 'Portachiavi in alluminio, incisione a laser o stampa a colori del tuo logo, dimensioni: 2, 6 x 9 cm. Peso: solo 16 gr. con scatola singola'
-        },
-
-        {
-            'ahref': '../Categorie/Braccialetti-personalizzati.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/bracciali-in-silicone.jpg',
-            'imgAlt': 'braccialetti in silicone personalizzati',
-            'prezzoVetrina': 'da € 0,66',
-            'titolo': 'Braccialetti in silicone',
-            'descrizione': 'Braccialetti in silicone a più colori, stampa a 1 colore inclusa'
-        },
-
-        {
-            'ahref': '../Categorie/Lanyard-personalizzati.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/lanyards-personalizzati.jpg',
-            'imgAlt': 'Lanyards personalizzati con logo fronte e retro',
-            'prezzoVetrina': 'da € 0,60',
-            'titolo': 'Lanyards personalizzati',
-            'descrizione': 'Lanyards personalizzato con il tuo logo aziendale stampato interno / esterno - Misura: 2 x 45-50 cm.'
-        },
-
-        {
-            'ahref': '../Categorie/Borse-Zaini-personalizzati.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/sacche economiche.jpg',
-            'imgAlt': 'sacche personalizzate',
-            'prezzoVetrina': 'da € 0,85',
-            'titolo': 'Sacca in nylon',
-            'descrizione': 'Dimensione cm 33x45 ca, doppia corda di chiusura, stampa a 1 colore su un lato'
-        },
-
-        {
-            'ahref': '../Categorie/Shopper-personalizzati.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/shopper cotone.jpg',
-            'imgAlt': 'shopper in cotone personalizzati',
-            'prezzoVetrina': 'da € 0,90',
-            'titolo': 'Shoppers in cotone',
-            'descrizione': 'Shopper in cotone naturale con manici colorati.F.to 40 x 30 cm. Personalizzati a 1 colore'
-        },
-
-        {
-            'ahref': '../Categorie/Borse-Zaini-personalizzati.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/cartella-porta-documenti.jpg',
-            'imgAlt': 'cartella porta documenti personalizzata a 1 colore',
-            'prezzoVetrina': 'da € 1,60',
-            'titolo': 'Cartella porta documenti',
-            'descrizione': 'Pratica cartella porta documenti, robusta in tessuto pesante personalizzata a 1 colore'
-        },
-
-        {
-            'ahref': '../Categorie/Braccialetti-personalizzati.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/bracciali-in-silicone-in-rilievo.jpg',
-            'imgAlt': 'braccialetti in silicone personalizzati in rilievo con lo stesso colore',
-            'prezzoVetrina': 'da € 0,78',
-            'titolo': 'Braccialetti in silicone',
-            'descrizione': 'Braccialetti in silicone a più colori, personalizzati in rilevo con lo stesso colore del braccialetto'
-        },
-
-        {
-            'ahref': '../Categorie/Braccialetti-personalizzati.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/bracciali-in-silicone-incisi.jpg',
-            'imgAlt': 'braccialetti in silicone personalizzati incisi con lo stesso colore',
-            'prezzoVetrina': 'da € 0,75',
-            'titolo': 'Braccialetti in silicone',
-            'descrizione': 'Braccialetti in silicone a più colori, personalizzati con incisione con lo stesso colore del braccialetto'
-        },
-
-        {
-            'ahref': '../Categorie/magliette-personalizzate.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/maglietta cotone bianca.jpg',
-            'imgAlt': 'maglietta cotone bianca personalizzata a 1 colore',
-            'prezzoVetrina': 'da € 2,30',
-            'titolo': 'Maglietta bianca personalizzata',
-            'descrizione': 'Maglietta bianca adulto / bambino 135 gr. in cotone 100%, girocollo a costina rinforzata, stampa a 1 colore da un lato'
-        },
-
-        {
-            'ahref': '../Categorie/magliette-personalizzate.aspx',
-            'imgSrc': '../../jmg/immaginiVetrina/maglietta cotone colorate.jpg',
-            'imgAlt': 'maglietta cotone colorata personalizzata a 1 colore',
-            'prezzoVetrina': 'da € 2,70',
-            'titolo': 'Maglietta colorata personalizzata',
-            'descrizione': 'Adulto / bambino 140 gr. in cotone 100%, 10 colori a scelta, girocollo a costina rinforzata, stampa a 1 colore da un lato'
         }
         
     ];
